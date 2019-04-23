741dfd77ff4b885d2ff3e5e0990ccefd95800354
Dev Davis <sean.dev.davis@gmail.com>
2019-04-22 21:22:21 -0500

    almost done with filtering

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -26,11 +26,11 @@ const shootTimeClass = (shootTime) => {
   return selectedClass;
 };
 
-const domStringBuilder = () => {
+const domStringBuilder = (locArray) => {
   // make string
   let domString = '';
   domString += '<div class="row">';
-  locations.forEach((location) => {
+  locArray.forEach((location) => {
     domString += '<div class="col-3">';
     domString += `<div class="card" id="${location.id}">`;
     domString += `<img src="${location.imageUrl}" class="card-img-top">`;
@@ -45,14 +45,58 @@ const domStringBuilder = () => {
   util.printToDom('locations', domString);
   // printToDom('locations', domString);
 };
+const filter = (e) => {
+  const buttonId = e.target.id;
+
+  const dark = locations.filter(x => x.shootTime === 'After Dark');
+  const all = locations.filter(x => x.shootTime === 'All');
+  const morning = locations.filter(x => x.shootTime === 'Morning');
+  const afternoon = locations.filter(x => x.shootTime === 'Afternoon');
+  const evening = locations.filter(x => x.shootTime === 'Evening');
+
+  switch (buttonId) {
+    case 'dark':
+      domStringBuilder(dark);
+      break;
+    case 'all':
+      domStringBuilder(all);
+      break;
+    case 'morning':
+      domStringBuilder(morning);
+      break;
+    case 'afternoon':
+      domStringBuilder(afternoon);
+      break;
+    case 'evening':
+      domStringBuilder(evening);
+      break;
+    default: domStringBuilder(locations);
+  }
+};
+
+const filterByTextEvent = (e) => {
+  const searchText = e.target.value;
+  const searchLocations = locations.filter((x) => {
+    const hasName = x.name.includes(searchText);
+    const hasAddress = x.address.includes(searchText);
+    return hasName || hasAddress;
+  });
+  domStringBuilder(searchLocations);
+};
 
 const initializeLocations = () => {
-// make an axios call
+  // make an axios call
   locationsData.getLocationsData()
     .then((resp) => {
       const locationsResults = resp.data.locations;
       locations = locationsResults;
-      domStringBuilder();
+      domStringBuilder(locations);
+      document.getElementById('all').addEventListener('click', filter);
+      document.getElementById('morning').addEventListener('click', filter);
+      document.getElementById('afternoon').addEventListener('click', filter);
+      document.getElementById('evening').addEventListener('click', filter);
+      document.getElementById('dark').addEventListener('click', filter);
+      document.getElementById('searchBox').addEventListener('click', filterByTextEvent);
     })
     .catch(err => console.error(err));
 };
