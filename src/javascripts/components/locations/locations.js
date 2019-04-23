83ba207f3e452810a0c7fc8c44dff956d22003a0
Dev Davis <sean.dev.davis@gmail.com>
2019-04-22 19:16:55 -0500

    location cards

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -8,9 +8,19 @@ let locations = [];
 const domStringBuilder = () => {
   // make string
   let domString = '';
+  domString += '<div class="row">';
   locations.forEach((location) => {
-    domString += `;<h4>${location.name}</h4>`;
+    domString += '<div class="col-3">';
+    domString += `<div class="card" id="${location.id}">`;
+    domString += `<img src="${location.imageUrl}" class="card-img-top">`;
+    domString += '<div class="card-body">';
+    domString += `<h4 class="card-header">${location.name}</h4>`;
+    domString += `<p class="card-text">${location.address}</p>`;
+    domString += '</div>';
+    domString += '</div>';
+    domString += '</div>';
   });
+  domString += '<div>';
   util.printToDom('locations', domString);
   // printToDom('locations', domString);
 };
